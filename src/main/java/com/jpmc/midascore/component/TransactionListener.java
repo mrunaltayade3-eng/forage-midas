6bb398adf5048a8086a92b4e0bcc31b53f720022
mrunaltayade3-eng <mrunaltayade3@gmail.com>
2026-07-25 18:31:34 -0500

    Update TransactionListener.java

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,50 +1,19 @@
-package com.jpmc.midascore.foundation;
+package com.jpmc.midascore.component;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class Transaction {
-    private long senderId;
-    private long recipientId;
-    private float amount;
+@Component
+public class TransactionListener {
+    private final DatabaseConduit databaseConduit;
 
-    public Transaction() {
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
     }
 
-    public Transaction(long senderId, long recipientId, float amount) {
-        this.senderId = senderId;
-        this.recipientId = recipientId;
-        this.amount = amount;
-    }
-
-    public long getSenderId() {
-        return senderId;
-    }
-
-    public void setSenderId(long senderId) {
-        this.senderId = senderId;
-    }
-
-    public long getRecipientId() {
-        return recipientId;
-    }
-
-    public void setRecipientId(long recipientId) {
-        this.recipientId = recipientId;
-    }
-
-    public float getAmount() {
-        return amount;
-    }
-
-    public void setAmount(float amount) {
-        this.amount = amount;
-    }
-
-    @Override
-    public String toString() {
-        return "Transaction {senderId=" + senderId
-                + ", recipientId=" + recipientId
-                + ", amount=" + amount + "}";
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
+    public void receive(Transaction transaction) {
+        databaseConduit.processTransaction(transaction);
     }
 }
