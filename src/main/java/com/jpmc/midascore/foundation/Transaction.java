f512dff94b6feedb7350369788dab577fb2fa5b7
mrunaltayade3-eng <mrunaltayade3@gmail.com>
2026-07-25 18:31:54 -0500

    Update Transaction.java

--- src/main/java/com/jpmc/midascore/foundation/Transaction.java
+++ src/main/java/com/jpmc/midascore/foundation/Transaction.java
@@ -1,40 +1,53 @@
-package com.jpmc.midascore.entity;
+package com.jpmc.midascore.foundation;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity
-public class TransactionRecord {
-
-    @Id
-    @GeneratedValue
-    private long id;
-
-    @ManyToOne
-    private UserRecord sender;
-
-    @ManyToOne
-    private UserRecord recipient;
-
-    @Column(nullable = false)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Transaction {
+    private long senderId;
+    private long recipientId;
     private float amount;
 
-    @Column(nullable = false)
-    private float incentive;
-
-    protected TransactionRecord() {
+    public Transaction() {
     }
 
-    public TransactionRecord(
-            UserRecord sender,
-            UserRecord recipient,
-            float amount,
-            float incentive) {
-        this.sender = sender;
-        this.recipient = recipient;
+    public Transaction(long senderId, long recipientId, float amount) {
+        this.senderId = senderId;
+        this.recipientId = recipientId;
         this.amount = amount;
-        this.incentive = incentive;
     }
 
+    public long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(long senderId) {
+        this.senderId = senderId;
+    }
+
+    public long getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(long recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction {senderId=" + senderId
+                + ", recipientId=" + recipientId
+                + ", amount=" + amount + "}";
+    }
+}
     public float getAmount() {
         return amount;
     }
