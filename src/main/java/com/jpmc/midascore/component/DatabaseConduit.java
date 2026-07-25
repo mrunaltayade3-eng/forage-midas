ff6dbedc92dcb46bb018f3d9b8ebf82f8bfd6ad0
mrunaltayade3-eng <mrunaltayade3@gmail.com>
2026-07-25 18:26:07 -0500

    Update DatabaseConduit.java

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,70 +1,50 @@
-package com.jpmc.midascore.component;
+package com.jpmc.midascore.foundation;
 
-import com.jpmc.midascore.entity.TransactionRecord;
-import com.jpmc.midascore.entity.UserRecord;
-import com.jpmc.midascore.foundation.Incentive;
-import com.jpmc.midascore.foundation.Transaction;
-import com.jpmc.midascore.repository.TransactionRepository;
-import com.jpmc.midascore.repository.UserRepository;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Component
-public class DatabaseConduit {
-    private final UserRepository userRepository;
-    private final TransactionRepository transactionRepository;
-    private final RestTemplate restTemplate;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Transaction {
+    private long senderId;
+    private long recipientId;
+    private float amount;
 
-    public DatabaseConduit(
-            UserRepository userRepository,
-            TransactionRepository transactionRepository,
-            RestTemplateBuilder builder) {
-        this.userRepository = userRepository;
-        this.transactionRepository = transactionRepository;
-        this.restTemplate = builder.build();
+    public Transaction() {
     }
 
-    public void save(UserRecord userRecord) {
-        userRepository.save(userRecord);
+    public Transaction(long senderId, long recipientId, float amount) {
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+        this.amount = amount;
     }
 
-    @Transactional
-    public void processTransaction(Transaction transaction) {
-        UserRecord sender = userRepository.findById(transaction.getSenderId());
-        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
-
-        if (sender == null || recipient == null
-                || sender.getBalance() < transaction.getAmount()) {
-            return;
-        }
-
-        Incentive incentive = restTemplate.postForObject(
-                "http://localhost:33433/incentive",
-                transaction,
-                Incentive.class);
-
-        float incentiveAmount = incentive == null ? 0 : incentive.getAmount();
-
-        sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(
-                recipient.getBalance() + transaction.getAmount() + incentiveAmount);
-
-        userRepository.save(sender);
-        userRepository.save(recipient);
-        transactionRepository.save(new TransactionRecord(
-                sender,
-                recipient,
-                transaction.getAmount(),
-                incentiveAmount));
+    public long getSenderId() {
+        return senderId;
     }
-}        sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
 
-        userRepository.save(sender);
-        userRepository.save(recipient);
-        transactionRepository.save(
-                new TransactionRecord(sender, recipient, transaction.getAmount()));
+    public void setSenderId(long senderId) {
+        this.senderId = senderId;
+    }
+
+    public long getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(long recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction {senderId=" + senderId
+                + ", recipientId=" + recipientId
+                + ", amount=" + amount + "}";
     }
 }
