dbaa0ca962d6e2476febdd2254aff6d6c7956494
mrunaltayade3-eng <mrunaltayade3@gmail.com>
2026-07-25 18:27:58 -0500

    Update TransactionListener.java

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,19 +1,50 @@
-package com.jpmc.midascore.component;
+package com.jpmc.midascore.foundation;
 
-import com.jpmc.midascore.foundation.Transaction;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Component
-public class TransactionListener {
-    private final DatabaseConduit databaseConduit;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Transaction {
+    private long senderId;
+    private long recipientId;
+    private float amount;
 
-    public TransactionListener(DatabaseConduit databaseConduit) {
-        this.databaseConduit = databaseConduit;
+    public Transaction() {
     }
 
-    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
-    public void receive(Transaction transaction) {
-        databaseConduit.processTransaction(transaction);
+    public Transaction(long senderId, long recipientId, float amount) {
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+        this.amount = amount;
+    }
+
+    public long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(long senderId) {
+        this.senderId = senderId;
+    }
+
+    public long getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(long recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction {senderId=" + senderId
+                + ", recipientId=" + recipientId
+                + ", amount=" + amount + "}";
     }
 }
