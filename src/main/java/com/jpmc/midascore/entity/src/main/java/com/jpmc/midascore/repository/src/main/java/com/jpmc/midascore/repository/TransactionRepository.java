91c37ef374e49401d2f261f89619233ab48674ff
mrunaltayade3-eng <mrunaltayade3@gmail.com>
2026-07-25 18:02:14 -0500

    Delete src/main/java/com/jpmc/midascore/entity/src/main/java/com/jpmc/midascore/repository/src/main/java/com/jpmc/midascore/repository/TransactionRepository.java

--- src/main/java/com/jpmc/midascore/entity/src/main/java/com/jpmc/midascore/repository/src/main/java/com/jpmc/midascore/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jpmc.midascore.repository;
-
-import com.jpmc.midascore.entity.TransactionRecord;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TransactionRepository extends CrudRepository<TransactionRecord, Long> {
-}
