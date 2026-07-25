6fb97fac22716b68ed61ff1f5ad9a04edaf6152d
mrunaltayade3-eng <mrunaltayade3@gmail.com>
2026-07-25 18:24:10 -0500

    Update DatabaseConduit.java

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -2,22 +2,28 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
 
     public DatabaseConduit(
             UserRepository userRepository,
-            TransactionRepository transactionRepository) {
+            TransactionRepository transactionRepository,
+            RestTemplateBuilder builder) {
         this.userRepository = userRepository;
         this.transactionRepository = transactionRepository;
+        this.restTemplate = builder.build();
     }
 
     public void save(UserRecord userRecord) {
@@ -34,7 +40,26 @@ public class DatabaseConduit {
             return;
         }
 
+        Incentive incentive = restTemplate.postForObject(
+                "http://localhost:33433/incentive",
+                transaction,
+                Incentive.class);
+
+        float incentiveAmount = incentive == null ? 0 : incentive.getAmount();
+
         sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(
+                recipient.getBalance() + transaction.getAmount() + incentiveAmount);
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+        transactionRepository.save(new TransactionRecord(
+                sender,
+                recipient,
+                transaction.getAmount(),
+                incentiveAmount));
+    }
+}        sender.setBalance(sender.getBalance() - transaction.getAmount());
         recipient.setBalance(recipient.getBalance() + transaction.getAmount());
 
         userRepository.save(sender);
